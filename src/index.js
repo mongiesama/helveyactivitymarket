9269b38a2e6d59fd1ce65462f464b6dd67c9eb34
Amanda Helman <amanda.helman@logmein.com>
2020-05-13 15:17:16 -0700

    Added code and note

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ const loadActivities = async () => {
   const response = await fetch(
     "https://j2e5ml6a36.execute-api.us-west-1.amazonaws.com/live/activities"
   );
-  // todo: add "code", create matching ledger
+  // todo: create matching ledger
   // todo: hide behind api.helveyactivitymarket.com (add cors filter?)
-  // todo: sort by order field
   // todo: allow re-sort by points?
   // todo: changing over time
   let activities = await response.json();
+  activities.sort(function(a,b){return a.order - b.order});
   let tableDom = document.getElementById("activityTable");
   activities.forEach(function(element, i) {
     console.log("adding activity " + element.name);
@@ -16,12 +16,19 @@ const loadActivities = async () => {
       bonus = element.bonus;
     }
     let row = tableDom.insertRow(i+1);
-    let nameCell = row.insertCell(0);
+    row.className = (i % 2 === 1) ? 'odd' : 'even';
+    let codeCell = row.insertCell(0);
+    codeCell.innerHTML = element.code;
+    let nameCell = row.insertCell(1);
     nameCell.innerHTML = element.name;
-    let pointsCell = row.insertCell(1);
+    if (element.note) {
+      nameCell.innerHTML = "<div class='tooltip'>" + element.name
+          + "<span class='tooltiptext wider'>" + element.note + "</span></div>";
+    }
+    let pointsCell = row.insertCell(2);
     pointsCell.innerHTML = element.points;
     pointsCell.className = 'pointsEntry';
-    let bonusCell = row.insertCell(2);
+    let bonusCell = row.insertCell(3);
     bonusCell.innerHTML = bonus;
   });
 };

1484e144025b8335a84c6ad8161608c2ba27bb3a
Amanda Helman <amanda.helman@logmein.com>
2020-05-13 11:24:22 -0700

    Got the table formatted nicely

--- src/index.js
+++ src/index.js
@@ -1,31 +1,30 @@
-import "./styles.css";
-
 const loadActivities = async () => {
   const response = await fetch(
     "https://j2e5ml6a36.execute-api.us-west-1.amazonaws.com/live/activities"
   );
+  // todo: add "code", create matching ledger
+  // todo: hide behind api.helveyactivitymarket.com (add cors filter?)
+  // todo: sort by order field
+  // todo: allow re-sort by points?
+  // todo: changing over time
   let activities = await response.json();
-  console.log("read all activities: " + activities);
-  let tableHtml = "<table><tr><th>Activity</th><th>Points per 15 minutes</th><th>Bonus Opportunity</th></tr>";
-  activities.forEach(element => {
+  let tableDom = document.getElementById("activityTable");
+  activities.forEach(function(element, i) {
     console.log("adding activity " + element.name);
-    var bonus = "";
+    let bonus = "";
     if (element.bonus) {
       bonus = element.bonus;
     }
-    tableHtml =
-      tableHtml +
-      "<tr><td>" +
-      element.name +
-      "</td><td>" +
-      element.points +
-      "</td><td>" +
-      bonus +
-      "</td></tr>";
-    console.log(tableHtml);
+    let row = tableDom.insertRow(i+1);
+    let nameCell = row.insertCell(0);
+    nameCell.innerHTML = element.name;
+    let pointsCell = row.insertCell(1);
+    pointsCell.innerHTML = element.points;
+    pointsCell.className = 'pointsEntry';
+    let bonusCell = row.insertCell(2);
+    bonusCell.innerHTML = bonus;
   });
-  tableHtml = tableHtml + "</table>";
-  document.getElementById("app").innerHTML = tableHtml;
-}
+};
 
+// noinspection JSIgnoredPromiseFromCall
 loadActivities();

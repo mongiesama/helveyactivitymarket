2b2e7d45e1658c4d7651ef36d1cdb41c28e0bff5
mongiesama <amanda.helman@gmail.com>
2020-05-13 08:43:43 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,31 @@
 import "./styles.css";
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+const loadActivities = async () => {
+  const response = await fetch(
+    "https://j2e5ml6a36.execute-api.us-west-1.amazonaws.com/live/activities"
+  );
+  let activities = await response.json();
+  console.log("read all activities: " + activities);
+  let tableHtml = "<table><tr><th>Activity</th><th>Points per 15 minutes</th><th>Bonus Opportunity</th></tr>";
+  activities.forEach(element => {
+    console.log("adding activity " + element.name);
+    var bonus = "";
+    if (element.bonus) {
+      bonus = element.bonus;
+    }
+    tableHtml =
+      tableHtml +
+      "<tr><td>" +
+      element.name +
+      "</td><td>" +
+      element.points +
+      "</td><td>" +
+      bonus +
+      "</td></tr>";
+    console.log(tableHtml);
+  });
+  tableHtml = tableHtml + "</table>";
+  document.getElementById("app").innerHTML = tableHtml;
+}
+
+loadActivities();
